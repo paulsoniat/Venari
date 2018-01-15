b9355f5f7a1f5d27a22f9cae24f1fdb0ffa87f3a
root <root@DESKTOP-QJKI47E.localdomain>
2018-01-15 12:11:46 -0600

    [chore] tweek database models - user fbId is now a string - badges now have a name column

--- database/models/Badge.js
+++ database/models/Badge.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 const db = require('../index');
 
 const Badge = db.define('badge', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+  },
   image: {
     type: Sequelize.STRING,
     allowNull: false,
