0e519712243b1c5ea078bb40a84f58c447eb985a
Paul Soniat <paulmsoniat@gmail.com>
2018-01-15 18:43:48 -0600

    [carousel] working dynamically renders challenge carousel, will work on adding click functionality

--- public/client/components/RotatingCarousel.jsx
+++ public/client/components/RotatingCarousel.jsx
@@ -6,32 +6,36 @@ import { green400, green600, blue400, blue600, red400, red600 } from 'material-u
 export default class RotatingCarousel extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      challengeData: [],
+    };
   }
+
   componentWillMount() {
     axios.get('/challenges').then((res) => {
-      console.log(res, "this is res")
-      this.setState({ challengeData: res.data });
+      this.setState({ challengeData: res.data, loaded: true });
     });
   }
 
 
   render() {
-    console.log('this is muh fuckin challenges', this.state.challengeData);
+    if (!this.state.loaded) return <div>Loading New Challenges</div>;
     return (
       <div>
         <AutoRotatingCarousel
-          label="Get started"
+          label="Begin this Challenge"
           open
+          mobile
+          style={{ position: 'inherit', width: '100%', height: '50%' }}
         >
           {this.state.challengeData.map(challenge =>
           (<Slide
             key={challenge.id}
             media={<img src={challenge.image} />}
-            mediaBackgroundStyle={challenge.mediaBackgroundStyle}
-            contentStyle={challenge.contentStyle}
+            mediaBackgroundStyle={green600}
+            contentStyle={green400}
             title={challenge.title}
-            subtitle={challenge.subtitle}
+            subtitle={challenge.description}
           />))}
         </AutoRotatingCarousel>
       </div>
