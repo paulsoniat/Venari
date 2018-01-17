7d7df0e5174b7aa921491840bf66b1ead6f8c510
root <root@DESKTOP-QJKI47E.localdomain>
2018-01-17 17:56:38 -0600

    [chore] fix merge conflict

--- public/client/components/index.jsx
+++ public/client/components/index.jsx
@@ -28,18 +28,6 @@ const AppRouter = () => (
       <Route path="/main" component={Main} />
       <Route path="/Gallery" component={Gallery} />
       <Route path="/Trophies" component={Trophies} />
-    </div>
-  </Router>
-);
-
-const AppRouter = () => (
-  <Router>
-    <div>
-      <h1><Link to="/main">Check out the Challenges!</Link></h1>
-      <h1><Link to="/leaderboard">Check out the Leaderboard!</Link></h1>
-      <Route exact path="/" component={Login} />
-      <Route path="/challenge/:id" component={Challenge} />
-      <Route path="/main" component={Main} />
       <Route path="/leaderboard" component={Leaderboard} />
     </div>
   </Router>
