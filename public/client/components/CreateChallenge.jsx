15bc5c349dfede252dc212cc17d859d24cf82978
root <root@DESKTOP-QJKI47E.localdomain>
2018-01-24 16:09:09 -0600

    [user-created-challenges] add error checking and warning

--- public/client/components/CreateChallenge.jsx
+++ public/client/components/CreateChallenge.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DatePicker from 'material-ui/DatePicker';
-import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
 import TextField from 'material-ui/TextField';
 import { List, ListItem } from 'material-ui/List';
 import IconButton from 'material-ui/IconButton';
@@ -11,23 +9,25 @@ import ActionDelete from 'material-ui/svg-icons/action/delete';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import RaisedButton from 'material-ui/RaisedButton';
-
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import Navbar from './Navbar.jsx';
 
 export default class CreateChallenge extends React.Component {
   constructor(props) {
     super(props);
-    const endDate = new Date();
-    endDate.setDate(endDate.getDate() + 7);
+    this.endDate = new Date();
+    this.endDate.setDate(this.endDate.getDate() + 7);
     this.state = {
       title: '',
       description: '',
       startDate: new Date(),
-      endDate,
+      endDate: this.endDate,
       image: '',
       items: [],
       item: '',
       autoOk: true,
+      open: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -35,6 +35,7 @@ export default class CreateChallenge extends React.Component {
     this.changeStart = this.changeStart.bind(this);
     this.changeEnd = this.changeEnd.bind(this);
     this.removeItem = this.removeItem.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   handleChange(e) {
@@ -58,29 +59,57 @@ export default class CreateChallenge extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    console.log('submit', this.state);
-    // axios.post('/challenge', this.state)
-    //   .then((response) => {
-    //     console.log('response', response);
-    //   });
+    // console.log('submit', this.state);
+    const {
+      title,
+      description,
+      image,
+      items,
+    } = this.state;
+    if (title !== '' && description !== '' && image !== '' && items.length > 0) {
+      axios.post('/challenge', this.state)
+        .then((response) => {
+          // console.log('response', response);
+          this.setState({
+            title: '',
+            description: '',
+            startDate: new Date(),
+            endDate: this.endDate,
+            image: '',
+            items: [],
+            item: '',
+          });
+        });
+    } else {
+      this.setState({
+        open: true,
+      });
+    }
   }
 
   addItem(e) {
     e.preventDefault();
-    const newItems = this.state.items;
-    newItems.push(this.state.item);
-    this.setState({
-      items: newItems,
-      item: '',
-    });
+    if (this.state.item !== '') {
+      const newItems = this.state.items;
+      newItems.push(this.state.item);
+      this.setState({
+        items: newItems,
+        item: '',
+      });
+    }
   }
 
   removeItem(index) {
-    this.shutup = 'fuck u eslint';
     const list = this.state.items;
     list.splice(index, 1);
     this.setState({
       items: list,
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      open: false,
     });
   }
 
@@ -89,7 +118,9 @@ export default class CreateChallenge extends React.Component {
       <MuiThemeProvider>
         <div>
           <Navbar history={this.props.history} />
+          <ErrorModal open={this.state.open} close={this.closeModal} />
           <div style={{ width: 300, margin: 'auto' }}>
+            {/* <h3>Create Challenge</h3> */}
             <TextField floatingLabelText="Title" name="title" value={this.state.title} onChange={this.handleChange} />
             <br />
             <TextField floatingLabelText="Description" name="description" value={this.state.description} onChange={this.handleChange} />
@@ -111,9 +142,10 @@ export default class CreateChallenge extends React.Component {
             <TextField floatingLabelText="Image URL" name="image" value={this.state.image} onChange={this.handleChange} />
             {/* <h4>Items:</h4> */}
             <TextField floatingLabelText="Add an Item" name="item" value={this.state.item} onChange={this.handleChange} />
-            <FloatingActionButton mini={true} onClick={this.addItem} >
+            <FloatingActionButton mini onClick={this.addItem} >
               <ContentAdd />
             </FloatingActionButton>
+            <p>Please test your items with <a href="https://visual-recognition-demo.ng.bluemix.net/" rel="noopener noreferrer" target="_blank">Watson</a> to make sure it recognizes the correct item</p>
             <List>
               {this.state.items.map((item, i) =>
                 (
@@ -133,3 +165,25 @@ export default class CreateChallenge extends React.Component {
     );
   }
 }
+
+const ErrorModal = ({ open, close }) => {
+  const actions = [
+    <FlatButton
+      label="Okay"
+      primary
+      keyboardFocused
+      onClick={close}
+    />,
+  ];
+  return (
+    <Dialog
+      title="Error!"
+      actions={actions}
+      modal={false}
+      open={open}
+      onRequestClose={close}
+    >
+      <p>Please Complete All Fields</p>
+    </Dialog>
+  );
+};
